7964da99b80b24e62b4adc33fdfedc4cf63dbeef
Chris Foxley-Evans <cfoxleyevans.compsci@gmail.com>
2014-09-12 12:10:01 +0100

    Update WebApplicationConfiguration.java

--- src/main/java/com/chrisfoxleyevans/WebApplicationConfiguration.java
+++ src/main/java/com/chrisfoxleyevans/WebApplicationConfiguration.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * Created by foxley-evansc on 12/09/2014.
- */
-
 @Configuration
 @ComponentScan("com.chrisfoxleyevans")
 @EnableWebMvc
